2cde10d20ac119bd95c5799e09bfeadd950f452a
Rachid <rachid@qraft.nl>
2015-08-11 17:27:40 +0200

    First attempt to update: still needs some work

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ module.exports = {
     app.import('vendor/css/ember-cli-framework7.css');
 
     var bowerDirectory = app.bowerDirectory;
-    app.import(bowerDirectory + '/framework7/dist/css/framework7.css');
-    app.import(bowerDirectory + '/framework7/dist/css/framework7.themes.css');
+    app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.css');
+    app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.colors.css');
     app.import(bowerDirectory + '/framework7/dist/js/framework7.js');
     app.import(bowerDirectory + '/framework7/dist/js/framework7.js.map', {
       destDir: 'assets'

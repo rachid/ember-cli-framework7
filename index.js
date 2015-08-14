42b72fc2276b2ad193851be315d509c355ccfd19
Rachid <rachid@qraft.nl>
2015-08-14 14:46:22 +0200

    using material

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ module.exports = {
     app.import('vendor/css/ember-cli-framework7.css');
 
     var bowerDirectory = app.bowerDirectory;
-    app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.css');
-    app.import(bowerDirectory + '/framework7/dist/css/framework7.ios.colors.css');
+    app.import(bowerDirectory + '/framework7/dist/css/framework7.material.css');
+    app.import(bowerDirectory + '/framework7/dist/css/framework7.material.colors.css');
     app.import(bowerDirectory + '/framework7/dist/js/framework7.js');
     app.import(bowerDirectory + '/framework7/dist/js/framework7.js.map', {
       destDir: 'assets'
